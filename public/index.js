c9e927c957ecab9c33af1bf2c6f7cc3cfd631a9a
Michael Lei <michaelleie0ca@gmail.com>
2023-03-18 18:09:33 -0700

    Add a default weather to display

--- public/index.js
+++ public/index.js
@@ -45,3 +45,36 @@ btnGetWeather.addEventListener("click", async (e) => {
 		weatherInfo.innerHTML = `<h2 class="text-center">${err.message}</h2>`;
 	}
 });
+
+(async () => {
+	try {
+		const response = await fetch(`/api/weather/vancouver`);
+		const weatherData = await response.json();
+		if (weatherData.cod === "404") {
+			throw new Error(`${userCity} is not a valid city.`);
+		}
+		console.log(weatherData);
+		const city = weatherData.name;
+		const country = weatherData.sys.country;
+		const temp = Math.floor(weatherData.main.temp);
+		const feelsLike = Math.floor(weatherData.main.feels_like);
+		const humidity = weatherData.main.humidity;
+		const description = weatherData.weather[0].description;
+		const icon = weatherData.weather[0].icon;
+		const iconURL = `http://openweathermap.org/img/wn/${icon}.png`;
+		weatherInfo.innerHTML = `
+		<div>
+			<h6>${city}, ${country}</h6>
+			<h3>${titleCase(description)}</p>
+			<img src="${iconURL}" alt="${description}">
+			<h2>${temp} °C</h2>
+		</div>
+		<div>
+			<p>Feels like: ${feelsLike} °C</p>
+			<p>Humidity: ${humidity}%</p>
+		</div>
+		`;
+	} catch (err) {
+		weatherInfo.innerHTML = `<h2 class="text-center">${err.message}</h2>`;
+	}
+})();
